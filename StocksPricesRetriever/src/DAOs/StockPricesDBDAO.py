1a71425a59eda3866d1d5efb7cada5036651dcb8
Elias <spanoselias@outlook.com>
2019-08-30 20:45:31 +0300

    1) Improve DBUtils functions.

--- StocksPricesRetriever/src/DAOs/StockPricesDBDAO.py
+++ StocksPricesRetriever/src/DAOs/StockPricesDBDAO.py
@@ -8,27 +8,19 @@ from StocksPricesRetriever.src.Objects.FetchStock import FetchStock
 
 
 def getStockPrice(conn, pricesSchema, date_added):
-    # create a cursor
-    cur = conn.cursor()
 
     fetchQuery = \
         "select symbol, price, date_added from %s.StocksPrices where date_added >= '%s'" % \
         (pricesSchema, date_added)
 
-    stockPricesList = []
     try:
-        cur.execute(fetchQuery)
-        rs = cur.fetchall()
-
-        for row in rs:
-            stockPriceRow = FetchStock(row)
-            stockPricesList.append(stockPriceRow)
+        resultList = DBUtils.executeSelectQuery(conn, fetchQuery, FetchStock)
 
     except:
         print(traceback.print_exc())
         conn.rollback()
 
-    return stockPricesList
+    return resultList
 
 
 stockPriceList = getStockPrice(DBUtils.getPostgresConnection(), 'pricesarchiver',
