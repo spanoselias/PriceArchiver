a3d91eb8c85257267f163a7da427d8873247c741
Elias <spanoselias@outlook.com>
2019-08-30 20:51:22 +0300

    1) Remove redundant packages

---
+++ src/Utils/DBUtils.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+
+def getPostgresConnection():
+    return psycopg2.connect("dbname=postgres user=postgres password=docker")
+
+
+def executeSelectQuery(conn, query, constructorObj):
+    # create a cursor
+    cur = conn.cursor()
+
+    cur.execute(query)
+    resutSet = cur.fetchall()
+
+    return collectResults(resutSet, constructorObj)
+
+
+def collectResults(resutSet, constructorObj):
+    streamList = []
+    for rs in resutSet:
+        obj = constructorObj(rs)
+        streamList.append(obj)
+
+    return streamList
