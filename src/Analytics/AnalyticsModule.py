85f998397d572d881b86853bb60621aa3e257881
Elias <spanoselias@outlook.com>
2019-09-07 12:36:28 +0300

    1) Aggregate analytics

--- src/Analytics/AnalyticsModule.py
+++ src/Analytics/AnalyticsModule.py
@@ -1,11 +1,21 @@
 import datetime
+from functools import reduce
 
 from src.DAOs import StockPricesDBDAO
 from src.Utils import DBUtils
 
-def maxPricePerStock():
-    stockPriceList = \
-        StockPricesDBDAO.getStockPrice(
-            DBUtils.getPostgresConnection(),
-            'pricesarchiver',
-            datetime.datetime.utcnow() - datetime.timedelta(days=0.1))
+
+def maxMinPricePerStock():
+    stockPriceList = StockPricesDBDAO.getStockPrice(
+        DBUtils.getPostgresConnection(),
+        'pricesarchiver',
+        datetime.datetime.utcnow() - datetime.timedelta(days=0.2))
+
+    stockPricesList = stockPriceList
+    maxStock = max(stockPricesList, key=lambda stock: stock.getPrice())
+    minStock = min(stockPricesList, key=lambda stock: stock.getPrice())
+
+    return maxStock, minStock
+
+res = maxMinPricePerStock()
+print(res)
